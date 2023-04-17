77f5c761d02edf3aa593219bcb528d36da32683e
Surina-Singh <surinsingh@hotmail.co.za>
2023-04-17 02:23:48 -0700

    file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""This module defines a class to manage file storage for hbnb clone"""
+""" Class to manage file storage for AirBnB clone """
 import json
 
 
 class FileStorage:
-    """This class manages storage of hbnb models in JSON format"""
+    """ Class manages storage of AirBnB models in JSON format """
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
-        """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+    def all(self, cls=None):
+        """ Returns a dictionary of models currently in storage """
+        if cls is None:
+            return FileStorage.__objects
+        my_dict = {}
+        for key, val in FileStorage.__objects.items():
+            if isinstance(val, cls):
+                my_dict[key] = val
+        return my_dict
 
     def new(self, obj):
-        """Adds new object to storage dictionary"""
+        """ Adds new object to storage dictionary """
         self.all().update({obj.to_dict()['__class__'] + '.' + obj.id: obj})
 
     def save(self):
-        """Saves storage dictionary to file"""
+        """ Saves storage dictionary to file """
         with open(FileStorage.__file_path, 'w') as f:
             temp = {}
             temp.update(FileStorage.__objects)
@@ -26,7 +32,7 @@ class FileStorage:
             json.dump(temp, f)
 
     def reload(self):
-        """Loads storage dictionary from file"""
+        """ Loads storage dictionary from file """
         from models.base_model import BaseModel
         from models.user import User
         from models.place import Place
@@ -48,3 +54,20 @@ class FileStorage:
                         self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
+    def delete(self, obj=None):
+        """ Public instance method to delete obj from __objects
+        if available
+        """
+        if obj is None:
+            return
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        if key in FileStorage.__objects:
+            del FileStorage.__objects[key]
+            self.save()
+
+    def close(self):
+        """ Public method to call reload for deserializing the
+        JSON file.
+        """
+        self.reload()
